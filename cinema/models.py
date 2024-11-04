8f0119d7c3cb34367f70b378ed68b8f401802bbb
alexpanzhar <alex.panzhar1@gmail.com>
2024-11-04 22:57:38 +0100

    feat: implement an API that provides CRUD operations for the Movie model

--- cinema/models.py
+++ cinema/models.py
@@ -6,3 +6,6 @@ class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     duration = models.IntegerField()
+
+    def __str__(self):
+        return f"Movie: {self.title}"
